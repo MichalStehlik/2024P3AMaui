0b78f4fbe44eb3d8afc93297ea6434f9043305e3
Michal Stehlík <michal.stehlik@pslib.cz>
2024-11-14 09:24:08 +0100

    clock

---
+++ Clock/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Bumptech.Glide.Util;
+
+namespace Clock
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        private bool OnTimerTick()
+        {
+            this.Dispatcher.Dispatch(() => { lblClock.Text = DateTime.Now.ToString("HH:mm:ss"); });
+            return true;
+            /*
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            });
+            return true;
+            */
+        }
+    }
+
+}
