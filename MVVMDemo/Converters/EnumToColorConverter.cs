b466f0a63998468e87412a68ac7b75c39dd81897
Michal Stehlík <michal.stehlik@pslib.cz>
2024-12-05 10:38:15 +0100

    converter

---
+++ MVVMDemo/Converters/EnumToColorConverter.cs
@@ -0,0 +1,44 @@
+﻿using MVVMDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.Convertors
+{
+    class EnumToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            if (value is ColorSet)
+            {
+                switch (value)
+                {
+                    case ColorSet.Black:
+                        return Colors.Black;
+                    case ColorSet.Blue:
+                        return Colors.Blue;
+                    case ColorSet.Green:
+                        return Colors.Green;
+                    case ColorSet.Red:
+                        return Colors.Red;
+                    default:
+                        return Colors.Black;
+
+                }
+            }
+            return Colors.Black;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MVVMDemo/ViewModels/FirstViewModel.cs
+++ MVVMDemo/ViewModels/FirstViewModel.cs
@@ -11,12 +11,71 @@ namespace MVVMDemo.ViewModels
     internal class FirstViewModel : INotifyPropertyChanged
     {
         private string _text = "Ukázkový text";
+        private int _size = 12;
+        private ColorSet _color = ColorSet.Black;
+
+        public FirstViewModel()
+        {
+            IncreaseCommand = new Command(
+                () => Size++,
+                () => Size < 72
+            );
+            DecreaseCommand = new Command(
+                () => Size--,
+                () => Size > 6
+            );
+            IncreaseByValueCommand = new Command<string>(
+                (value) => {
+                    int x = Int32.Parse(value);
+                    Size += x;
+                },
+                (value) =>
+                {
+                    int x = Int32.Parse(value);
+                    return (Size + x) < 72 && (Size - x) > 6;
+                }
+            );
+            ChangeColorCommand = new Command<string>(
+                (value) =>
+                {
+                    Color = (ColorSet)Enum.Parse(typeof(ColorSet), value);
+                }
+            );
+        }
+
+        public Command IncreaseCommand {  get; set; }
+        public Command DecreaseCommand { get; set; }
+        public Command<string> IncreaseByValueCommand { get; set; }
+        public Command<string> ChangeColorCommand { get; set; }
         public string Text { 
             get { return _text; } 
             set { 
                 _text = value;
                 OnPropertyChanged();
             } 
+        }
+
+        public int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                OnPropertyChanged();
+                IncreaseCommand.ChangeCanExecute();
+                DecreaseCommand.ChangeCanExecute();
+                IncreaseByValueCommand.ChangeCanExecute();
+            }
+        }
+
+        public ColorSet Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                OnPropertyChanged();
+            }
         }
 
         #region MVVM
@@ -27,5 +86,13 @@ namespace MVVMDemo.ViewModels
 
         public event PropertyChangedEventHandler? PropertyChanged;
         #endregion
+    }
+
+    enum ColorSet
+    {
+        Black,
+        Red,
+        Green,
+        Blue
     }
 }
